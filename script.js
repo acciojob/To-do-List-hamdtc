b25c5a7671dcdc5d30072bc7481604836b923b74
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-02-19 07:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
 
+addTodoBtn.addEventListener("click", function() {
+    let todo = newTodoInput.value;
+    if(!todo.trim()){
+    alert("Please enter a valid todo item");
+    return;
+    }
+    let newTodo = document.createElement("li");
+    newTodo.innerHTML = todo;
+    todoList.appendChild(newTodo);
+    newTodoInput.value = "";
+});
