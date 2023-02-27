d89f31b6e7a4ae6dca93eba62f5bfdb7e01d4875
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-02-27 07:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-var todo=document.getElementById('newTodoInput');
-var list=document.getElementById('todoList');
-var btn=document.getElementById('addTodoBtn');
-
+var todo=document.getElementById("newTodoInput");
+var list=document.getElementById("todoList");
+var btn=document.getElementById("addTodoBtn");
+//alert(btn)
 function addTodo(){
 	var todoVal=todo.value();
+	
+	alert("valu");
 
 	if(todoVal){
 		var li=document.createElement('li');
+		li.textContent=todoVal;
 		list.appendChild(li);
 		todo.value="";
 	}
 }
-btn.addEventListner("click",addTodo);
+btn.addEventListner('click',addTodo);
+
 
